b6411bff5c3ed94cc4f1d156f4c39b6b3ea43bc6
Senthil Kumar Shanmugha Sundaram <senthsha@gmail.com>
2021-12-29 20:31:09 +0000

    test

--- models/staging/jaffle_shop/stg_customers.sql
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -1,6 +1,7 @@
-select
+with customers as ( select
     id as customer_id,
     first_name,
     last_name
 
-from raw.jaffle_shop.customers
+from raw.jaffle_shop.customers )
+select * from customers
--- models/staging/jaffle_shop/stg_orders.sql
+++ models/staging/jaffle_shop/stg_orders.sql
@@ -1,7 +1,8 @@
-select
+with orders as ( select
     id as order_id,
     user_id as customer_id,
     order_date,
     status
 
-from raw.jaffle_shop.orders
+from raw.jaffle_shop.orders)
+select * from orders
